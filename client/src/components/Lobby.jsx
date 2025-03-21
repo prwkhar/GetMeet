7d1faadec787f33fdc6ef71d8b7fa1e684b94858
prakhar <prakharagarwal1434@gmail.com>
2025-03-21 22:43:02 +0530

    added socket wrapper

--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useState } from 'react'
 
 function Lobby() {
+  const [formdata,setformdata]=useState({
+    email:"",
+    roomno: "",
+  })
+
+  const changehandler=(e)=>{
+    setformdata({
+      ...formdata,
+      [e.target.id]: e.target.value,
+    })
+  };
+
+  const handlesubmit = useCallback((e)=>{
+    e.preventDefault();
+    console.log(formdata.email);
+    console.log(formdata.roomno);
+  },[formdata])
+
   return (
     <div>
       <h1>
         Lobby
       </h1>
+      <form onSubmit={handlesubmit}>
+        <label htmlFor='email'>Email Id</label>
+        <input type='email' id='email'
+        value={formdata.email} onChange={changehandler}/>
+        <br/>
+        <label htmlFor='roomno'>Room no</label>
+        <input type='number' id='roomno' 
+        value={formdata.roomno} onChange={changehandler}/>
+        <button>Join Room</button>
+      </form>
     </div>
   )
 }
---
+++ client/src/context/SocketProvider.jsx
@@ -0,0 +1,20 @@
+import React,{createContext, useContext, useMemo} from 'react'
+import {io} from "socket.io-client"
+
+const socketcontext = createContext(null);
+
+export const useSocket = () =>{
+    const socket = useContext(socketcontext);
+    return socket; 
+};
+
+function SocketProvider({props}) {
+    const socket = useMemo(()=>io("localhost:8000"),[]);
+  return (
+    <socketcontext.Provider value={socket}>
+        {props.children}
+    </socketcontext.Provider>
+  )
+}
+
+export default SocketProvider
